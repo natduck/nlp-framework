92cf3d37dd71d22c1e7188d381c855c8a1e45fce
natduck <78576975+natduck@users.noreply.github.com>
2023-02-27 23:42:29 -0500

    Update sankey.py

--- sankey.py
+++ sankey.py
@@ -1,6 +1,12 @@
 """
 sankey.py: A reusable library for sankey visualizations
+
+Nathan Brito, Kelly Chen, Anh Nguyen, Tung Giang
+DS3500 / A Reusable Extensible Framework for Natural Language Processing
+Homework 3
+Date Created: 2/15/2023 / Date Last Updated: 2/27/2023
 """
+
 
 import plotly.graph_objects as go
 
