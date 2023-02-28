69b53f378648fa7f7da3116740b75335079b7083
natduck <78576975+natduck@users.noreply.github.com>
2023-02-27 22:18:53 -0500

    Comments

--- sankey.py
+++ sankey.py
@@ -7,18 +7,25 @@ import plotly.graph_objects as go
 
 def filter_top_words(df, k):
     """
-    Filters the k most common words in the
+    Filters the k most common words in each text 
     :param df: (int) this is what parameter 1 is
-    :param k: (int) Number of top words
-    :return:
+    :param k: (int) Number of common words
+    :return: (df) An edited dataframe that includes the top k common words in each text 
     """
+    # Create top_words_set 
     top_words_set = set()
+    
+    # Create a list of labels 
     labels_list = list(df["label"])
+    
+    # Go through each label/text and extract the top k words for each text and update top_words_set
     for label in labels_list:
         df_temp = df[df["label"] == label]
         df_temp = df_temp.sort_values("count", ascending=False)[:k]
         word_set = set(df_temp["word"])
         top_words_set.update(word_set)
+        
+    # Extract words that are in top_words_set
     df = df[df["word"].isin(top_words_set)]
     return df
 
@@ -29,7 +36,7 @@ def _code_mapping(df, src, targ):
   :param df (DataFrame): The dataframe
   :param src: (df) The source nodes of the Sankey diagram
   :param targ: (df) The target nodes of the Sankey diagram
-  :return: The DataFrame and the labels
+  :return: The DataFrame (df) and the labels 
   """
   # Get distinct labels
   labels = sorted(list(set(list(df[src]) + list(df[targ]))))
@@ -54,8 +61,12 @@ def make_sankey(df, src, targ, vals, word_list, k, **kwargs):
         columns (list): List of columns for the Sankey diagram
         vals (None/str): Name of the column where the values are for the Sankey diagram (str)
     """
+    # Check if user called a word_list
     if word_list == None:
+        # Look for top k words for each file 
         df = filter_top_words(df=df, k=k)
+        
+    # Else look for words from word_list
     else:
         df = df[df["word"].isin(word_list)]
 
@@ -77,7 +88,7 @@ def make_sankey(df, src, targ, vals, word_list, k, **kwargs):
     pad = kwargs.get("pad", 50)
     node = {"label": labels, "pad": pad}
 
-    # Initialize and display Sankey diagram
+    # Initialize and return Sankey diagram
     sk = go.Sankey(valueformat=".0f", link=link, node=node)
     fig = go.Figure(sk)
     return fig
