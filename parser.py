3fc353298581bc66182c8d64512c5d1c29a3dabc
natduck <78576975+natduck@users.noreply.github.com>
2023-02-27 23:42:15 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,3 +1,10 @@
+"""
+Nathan Brito, Kelly Chen, Anh Nguyen, Tung Giang
+DS3500 / A Reusable Extensible Framework for Natural Language Processing
+Homework 3
+Date Created: 2/15/2023 / Date Last Updated: 2/27/2023
+"""
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 stopwords_list = set(stopwords.words("english"))
